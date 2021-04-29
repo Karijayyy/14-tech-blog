835914b55199e0dbf01a88c66b081026e380b355
Kari Johnson <kari@crownmediamarketing.com>
2021-04-29 08:51:50 -0600

    linked server.js to other files

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const ehandlebars = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-let sequelize;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-module.exports = sequelize;
+// Set up Handlebars.js engine with custom helpers
+const hbs = ehandlebars.create({ helpers });
+
+// found in npmjs docs for connect-session-sequilize
+const sess = {
+  secret: 'keyboard cat',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// tells Express.js  which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on http://localhost:3001'));
+});
