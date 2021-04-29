363b078583d4ce10745bed82fcff131808bfe08b
Kari Johnson <kari@crownmediamarketing.com>
2021-04-29 08:54:16 -0600

    connected db

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// referenced my homework 13 for this code
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
