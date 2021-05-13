c10d97faffa22532e55f634f498c538d8c328118
Kari Johnson <kari@crownmediamarketing.com>
2021-05-13 17:06:59 -0600

    cleaned up file structure & added some styling

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-// const apiRoutes = require ('./api/')
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js'); 
 const commRoutes = require('./comment-routes.js')
@@ -8,6 +7,5 @@ const commRoutes = require('./comment-routes.js')
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes); 
 router.use('/comments', commRoutes)
-// router.use('/api', apiRoutes);
 
 module.exports = router;
